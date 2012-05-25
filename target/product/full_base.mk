e9249504fcb063344e197cbeb8bea87333fdf1e8
Derek Rodriguez <drod2169@gmail.com>
2012-05-25 10:31:50 -0400

    Yup

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -31,7 +31,7 @@ PRODUCT_PACKAGES := \
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.com.android.dateformat=MM-dd-yyyy \
     ro.config.ringtone=Ring_Synth_04.ogg \
-    ro.config.notification_sound=pixiedust.ogg
+    ro.config.notification_sound=proximal.ogg
 
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US
